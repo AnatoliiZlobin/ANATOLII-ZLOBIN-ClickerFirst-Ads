dde97c2af9c40d3715ecf7047b29ecbb8d62525b
DESKTOP-HCG4CHC\anato <anatoliy.zlobin1210@gmail.com>
2020-11-15 19:23:04 +0100

    ADS DONE

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Play : MonoBehaviour
+{
+    public Button adsButton;
+    [System.Obsolete]
+    void Awake()
+    {
+        adsButton.onClick.AddListener(adsButton_onClick);
+    }
+
+    [System.Obsolete]
+    void adsButton_onClick()
+    {
+        SceneManager.LoadScene("Scenes/Game");
+    }
+}
+
